4e15c822d6c5c1d0413ca8a7d5d66d3c9fa3e81c
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-06-04 13:42:59 +0430

    add modules node js

---
+++ node js - module/comman js/app.js
@@ -0,0 +1,11 @@
+const logger = require("./test");
+// const { log, planet } = require("./test");
+const { log, planet } = logger;
+
+
+log('john doe');
+
+console.log(planet);
+
+// console.log(logger);
+
---
+++ node js - module/es6/script2.mjs
@@ -0,0 +1,8 @@
+import userName from "./script1.mjs";
+import {printUser} from './script1.mjs'
+
+console.log(userName);
+
+printUser("thomas", "donner")
+
+// in terminal --> node --experimental-modules script.mjs
