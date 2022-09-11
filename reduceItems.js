c9bfeb879649b7b4d71bf0c1ede4070840682d5c
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-09-11 22:04:09 +0430

    add reduce item map add items

---
+++ reduceItems.js
@@ -0,0 +1,24 @@
+const items = [
+  {
+    name: 'بهرام',
+    id: 1,
+  },
+  {
+    name: 'آرش',
+    id: 2,
+  },
+  {
+    name: 'مهسا',
+    id: 3,
+  },
+  {
+    name: 'سهیلا',
+    id: 4,
+  }
+];
+
+const reduced = HistoryFields.reduce((acc, val) => (
+  {...acc, [val.id]: {name: val.name, college: false, id: val.id, major: ''}}
+), {})
+
+console.log(reduced)
