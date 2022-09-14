8339af4847d740580ab4e0134ed23ecebf83d1a2
Esrafil <57289402+EsrafilElahi@users.noreply.github.com>
2022-09-14 21:08:56 +0430

    add valid data state

--- validState.js
+++ validState.js
@@ -6,8 +6,18 @@ const data = {
   age: ""
 }
 
+const data2 = {
+  name: "a",
+  family: "a",
+  city: "a",
+  phone: "a",
+  age: "a",
+};
 
-const valid = [...Object.values(data)].every(item => !!item)
+
+const valid = Array.from(Object.values(data)).every((item) => !!item); // false
+const valid2 = [...Object.values(data2)].every((item) => !!item); // true
     
     
 console.log(valid)
+console.log(valid2)
