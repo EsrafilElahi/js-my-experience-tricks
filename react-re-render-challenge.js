0ad2f5dd93e95c0824cb34e7caf1fddcc4e753f0
Esrafil <essinho1121@gmail.com>
2023-11-18 13:11:44 +0330

    Create react-re-render-challenge.js

---
+++ react-re-render-challenge.js
@@ -0,0 +1,42 @@
+import React, {memo, useCallback, useMemo} from 'react';
+import './style.css'
+
+export function App() {
+  const [count, setCount] = React.useState(0)
+  const handleClick = () => setCount(c => c + 1)
+
+  // first way - memoize the value both of them
+  // const memoizedSecond = useMemo(() => (<Second />), []);
+  // const memoizedFirst = useMemo(() => (<First />), []);
+
+  return (
+    <div className='app'>
+      <button onClick={handleClick}>{`count: ${count}`}</button>
+      <MemoizedFirst>
+        <Second />
+      </MemoizedFirst>
+    </div>
+  )
+}
+
+const First = ({ children }) => {
+  React.useEffect(() => {
+    console.log('First rendered')
+  })
+  return <div>{children}</div>
+}
+
+
+const FirstAreEqual = (prev, next) => {
+  return prev.children !== next.children
+}
+
+// second way - memo the First component with custom equality function as second parameter (React.memo doesn't work with children prop)
+const MemoizedFirst = memo(First, FirstAreEqual)
+
+const Second = () => {
+  React.useEffect(() => {
+    console.log('Second rendered')
+  })
+  return null
+}
